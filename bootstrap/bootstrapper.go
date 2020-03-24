b59b11ad0065da4e767c6cae125022ace2e70d8c
pigzzz123 <205270006@qq.com>
2020-03-24 23:21:08 +0800

    fixed

--- bootstrap/bootstrapper.go
+++ bootstrap/bootstrapper.go
@@ -109,8 +109,8 @@ func (b *Bootstrapper) Bootstrap() *Bootstrapper {
 	b.SetupErrorHandlers()
 
 	// static files
-	//b.Favicon(StaticAssets + Favicon)
-	b.HandleDir(StaticAssets[1:len(StaticAssets)-1], StaticAssets)
+	b.HandleDir("/", StaticAssets)
+
 
 	// middleware, after static files
 	b.Use(recover.New())
